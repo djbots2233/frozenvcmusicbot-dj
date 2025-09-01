244a6740696609c71f4e69be515ecb8c435a4f6a
djbots2233 <aspgptc@gmail.com>
2025-09-01 19:35:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -378,7 +378,7 @@ async def start_handler(_, message):
     support_group = os.getenv("SUPPORT_GROUP", "https://t.me/Frozensupport1")
     start_animation = os.getenv(
         "START_ANIMATION",
-        "https://filehosting.kustbotsweb.workers.dev/eju.jpg"
+        "https://ibb.co/B5XK17yW"
     )
 
     caption = (
